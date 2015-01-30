f9de4432749fd8f54c3b3d264181ca9c65eb0142
El arte del programador <elartedelprogramador@gmail.com>
2015-01-30 21:09:53 +0100

    Now the portfolio reads the projects from a JSON file

--- dev/js/app.js
+++ dev/js/app.js
@@ -80,3 +80,12 @@ angularCorp.controller('menuToggleController',
         });
     }
 );
+
+angularCorp.controller('portfolioController', ['$scope', '$http', 
+    function ($scope, $http)
+    {
+        $http.get('../data/portfolio.json').success(function(data) {
+            $scope.projects = data;
+        });
+    }
+]);
--- test/testApp.js
+++ test/testApp.js
@@ -39,3 +39,101 @@ describe('menuController', function()
         expect(scope.menu[6].id).toBe('licencias');
     }));
 });
+
+describe('portfolioController', function()
+{
+    var scope;
+    beforeEach(module('angularCorp'));
+
+    beforeEach(inject(function(_$httpBackend_, $controller)
+    {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('../data/portfolio.json').
+            respond(
+                [
+                    {
+                        "name": "Proyecto 1",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 2",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 3",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 4",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 5",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 6",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 7",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    },
+                    {
+                        "name": "Proyecto 8",
+                        "image": "../img/portfolio.jpg",
+                        "url": "http://www.google.com"
+                    }
+                ]
+            );
+
+        scope = {};
+        $controller('portfolioController', {$scope:scope});
+    }));
+
+    it('should create "projects" with 8 projects', inject(function($controller) 
+    { 
+        expect(scope.projects).toBeUndefined();
+
+        $httpBackend.flush();
+        expect(scope.projects.length).toBe(8);
+    }));
+
+    it('should create "projects" with specific parameters', inject(function($controller) 
+    { 
+        expect(scope.projects).toBeUndefined();
+        
+        $httpBackend.flush();
+        expect(scope.projects[0].name).toBe('Proyecto 1');
+        expect(scope.projects[0].url).toBe('http://www.google.com');
+        expect(scope.projects[0].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[1].name).toBe('Proyecto 2');
+        expect(scope.projects[1].url).toBe('http://www.google.com');
+        expect(scope.projects[1].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[2].name).toBe('Proyecto 3');
+        expect(scope.projects[2].url).toBe('http://www.google.com');
+        expect(scope.projects[2].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[3].name).toBe('Proyecto 4');
+        expect(scope.projects[3].url).toBe('http://www.google.com');
+        expect(scope.projects[3].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[4].name).toBe('Proyecto 5');
+        expect(scope.projects[4].url).toBe('http://www.google.com');
+        expect(scope.projects[4].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[5].name).toBe('Proyecto 6');
+        expect(scope.projects[5].url).toBe('http://www.google.com');
+        expect(scope.projects[5].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[6].name).toBe('Proyecto 7');
+        expect(scope.projects[6].url).toBe('http://www.google.com');
+        expect(scope.projects[6].image).toBe('../img/portfolio.jpg');
+        expect(scope.projects[7].name).toBe('Proyecto 8');
+        expect(scope.projects[7].url).toBe('http://www.google.com');
+        expect(scope.projects[7].image).toBe('../img/portfolio.jpg');
+    }));
+});
